9a2fe3e9bd17fe5afde81e58824049308187675c
DELL <omnianabil1920@gmail.com>
2024-07-22 22:50:15 +0300

    getting user id and name

--- app/src/main/java/com/example/thejournalapp/AddJournalActivity.java
+++ app/src/main/java/com/example/thejournalapp/AddJournalActivity.java
@@ -3,12 +3,61 @@ package com.example.thejournalapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class AddJournalActivity extends AppCompatActivity {
+    private Button saveButton;
+    private ImageView addPhotoBtn;
+    private ProgressBar progressBar;
+    private EditText titleEditText;
+    private EditText thoughtsEditText;
+    private ImageView imageView;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference =
+            db.collection("Journal");
+    private StorageReference storageReference;
+    private String currentUserId;
+    private String currentUserName;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_journal);
+
+        progressBar = findViewById(R.id.post_progressBar);
+        titleEditText = findViewById(R.id.post_title_et);
+        thoughtsEditText = findViewById(R.id.post_description_et);
+        imageView = findViewById(R.id.post_imageView);
+        saveButton = findViewById(R.id.post_save_journal_button);
+        addPhotoBtn = findViewById(R.id.postCameraButton);
+        progressBar.setVisibility(View.INVISIBLE);
+        storageReference = FirebaseStorage.getInstance()
+                .getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (user != null){
+            currentUserId = user.getUid();
+            currentUserName = user.getDisplayName();
+        }
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
